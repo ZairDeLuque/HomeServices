3904de58bfee2becc7845e0a0b6b3603ec0c3f58
Alexander Santiago Vazquez <116230419+AlexanderSantiagoVazquez@users.noreply.github.com>
2023-09-12 09:19:46 -0600

    ignorar

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './bin/stages/home/home.component';
 import { InsideHomeComponent } from './bin/stages/inside-home/inside-home.component';
 import { TermsComponent } from './bin/stages/terms/terms.component';
 import { LoginComponent } from './bin/stages/login/login.component';
+import { ProfileComponent } from './bin/stages/profile/profile.component';
 
 const routes: Routes = [
   {path: 'start', component: HomeComponent},
   {path: 'home', component: InsideHomeComponent},
   {path: 'terms', component: TermsComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path:'perfil', component: ProfileComponent}
 ];
 
 @NgModule({
