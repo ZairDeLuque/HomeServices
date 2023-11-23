8a03cb01211e0e973d14255dd7650d418db63b82
Alexander Santiago Vazquez <116230419+AlexanderSantiagoVazquez@users.noreply.github.com>
2023-11-23 12:52:47 -0600

    PDFMaker

---
+++ back-end/bin/utility/pdfMakerUtilities/modelManager.pdf.js
@@ -0,0 +1,10 @@
+//PDF Model Manager 
+// HomeServices Web Services
+// @zairdeluque - The creator
+
+
+const pdfPackage = require('pdfmake');
+const file = require('fs');
+
+const Font = require('')
+
--- front-end/src/app/bin/stages/shops/shops.component.ts
+++ front-end/src/app/bin/stages/shops/shops.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-shops',
   templateUrl: './shops.component.html',
   styleUrls: ['./shops.component.css']
 })
-export class ShopsComponent {
+export class ShopsComponent implements OnInit{
+  protected items: MenuItem[];
 
+  constructor(){
+    this.items = [{
+      label: 'Filtros',
+      icon: 'bi bi-funnel',
+      items: [{
+        label: 'Ordenar alfabeticamente (A-Z)',
+        icon: 'bi bi-sort-alpha-up',
+      },
+      {
+        label: 'Ordenar alfabeticante (Z-A)',
+        icon: 'bi bi-sort-alpha-up-alt',
+      }
+    ]
+    }];
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
